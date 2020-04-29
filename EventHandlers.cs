f2ff96c46c6a900cb4e0fffd1ffb5810b4a0b2b1
kikohou <alvarodiazthompson@gmail.com>
2020-04-29 16:34:49 -0700

    First Commit

---
+++ EventHandlers.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EXILED;
+using EXILED.Extensions;
+
+namespace TKLimit
+{
+    class EventHandlers
+    {
+        private Plugin Plugin;
+        public EventHandlers(Plugin plugin) => Plugin = plugin;
+
+        internal void OnPlayerDamage(ref PlayerHurtEvent ev)
+        {
+            string plrClass = GetPlrClass(ev.Player.GetRole());
+            string atkClass = GetPlrClass(ev.Attacker.GetRole());
+            if (plrClass == atkClass)
+            {
+                if (Plugin.plrTKs[ev.Attacker.GetPlayerId()] >= Plugin.tkLimit)
+                {
+                    if (ev.Attacker.GetPlayerId() != ev.Player.GetPlayerId())
+                    {
+                        ev.Player.AddHealth(ev.Amount);
+                    }
+                }
+            }
+        }
+
+        internal void OnPlayerDeath(ref PlayerDeathEvent ev)
+        {
+            string plrClass = GetPlrClass(ev.Player.GetRole());
+            string kilClass = GetPlrClass(ev.Killer.GetRole());
+            if (plrClass == kilClass)
+            {
+                if (ev.Killer.GetPlayerId() != ev.Player.GetPlayerId())
+                {
+                    Plugin.plrTKs[ev.Killer.GetPlayerId()]++;
+                    if (Plugin.log == true)
+                    {
+                        if (Plugin.plrTKs[ev.Killer.GetPlayerId()] >= Plugin.tkLimit)
+                        {
+                            Log.Info($"{ev.Killer.GetNickname()} attempted to tk {ev.Player.GetNickname()} as {ev.Killer.GetRole()}");
+                        }
+                        else
+                        {
+                            Log.Info($"{ev.Killer.GetNickname()} has tked {ev.Player.GetNickname()} as {ev.Killer.GetRole()}");
+                        }
+                    }
+                }
+            }
+        }
+
+        internal void OnPlayerJoin(PlayerJoinEvent ev)
+        {
+            Plugin.plrTKs.Add(ev.Player.GetPlayerId(), 0);
+        }
+
+        internal void OnPlayerLeave(PlayerLeaveEvent ev)
+        {
+            Plugin.plrTKs.Remove(ev.Player.GetPlayerId());
+        }
+
+        public string GetPlrClass(RoleType plrRole)
+        {
+            string plrClass = null;
+            if (Plugin.NTF.Contains(plrRole))
+            {
+                plrClass = "NTF";
+            }
+            if (Plugin.CHAOS.Contains(plrRole))
+            {
+                plrClass = "CHAOS";
+            }
+            if (Plugin.SCP.Contains(plrRole))
+            {
+                plrClass = "SCP";
+            }
+            return plrClass;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EXILED;
+
+namespace TKLimit
+{
+    public class Plugin : EXILED.Plugin
+    {
+        private EventHandlers EventHandlers;
+
+        public static bool enabled;
+        public static int tkLimit;
+        public static bool log;
+        private static RoleType[] NTFArray = {RoleType.Scientist, RoleType.FacilityGuard, RoleType.NtfCadet, RoleType.NtfScientist, RoleType.NtfLieutenant, RoleType.NtfCommander};
+        private static RoleType[] CHAOSArray = {RoleType.ClassD, RoleType.ChaosInsurgency};
+        private static RoleType[] SCPArray = {RoleType.Scp049, RoleType.Scp0492, RoleType.Scp079, RoleType.Scp096, RoleType.Scp106, RoleType.Scp173, RoleType.Scp93953, RoleType.Scp93989};
+        public List<RoleType> NTF = new List<RoleType>(NTFArray);
+        public List<RoleType> CHAOS = new List<RoleType>(CHAOSArray);
+        public List<RoleType> SCP = new List<RoleType>(SCPArray);
+        public Dictionary<int, int> plrTKs = new Dictionary<int, int>();
+
+        public override string getName { get; } = "TKLimit";
+
+        public override void OnDisable()
+        {
+            Events.PlayerJoinEvent -= EventHandlers.OnPlayerJoin;
+            Events.PlayerLeaveEvent -= EventHandlers.OnPlayerLeave;
+            Events.PlayerHurtEvent -= EventHandlers.OnPlayerDamage;
+            Events.PlayerDeathEvent -= EventHandlers.OnPlayerDeath;
+
+            EventHandlers = null;
+        }
+
+        public void loadConfig()
+        {
+            enabled = Config.GetBool("tkl_enable", true);
+            tkLimit = Config.GetInt("tkl_limit", 4);
+            log = Config.GetBool("tkl_log", true);
+        }
+
+        public override void OnEnable()
+        {
+            loadConfig();
+            if (!enabled) { return; }
+
+            Log.Info("Starting TKLimiter...");
+            EventHandlers = new EventHandlers(this);
+
+            Events.PlayerJoinEvent += EventHandlers.OnPlayerJoin;
+            Events.PlayerLeaveEvent += EventHandlers.OnPlayerLeave;
+            Events.PlayerHurtEvent += EventHandlers.OnPlayerDamage;
+            Events.PlayerDeathEvent += EventHandlers.OnPlayerDeath;
+        }
+
+        public override void OnReload()
+        {
+            
+        }
+    }
+}
